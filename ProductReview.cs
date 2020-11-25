819c6f88458b5e2eec200011b4eccd8152a0ffae
kalyangoud145 <kalyangoud145@gmail.com>
2020-11-25 20:06:04 +0530

    [Kalyan] added product reviews as default values to list

---
+++ ProductReview.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductReviewManagement
+{
+    /// <summary>
+    /// POCO Class
+    /// </summary>
+    class ProductReview
+    {
+        public int ProductID { get; set; }
+        public int UserID { get; set; }
+        public double Rating { get; set; }
+        public string Review { get; set; }
+        public bool isLike { get; set; }
+        /// <summary>
+        /// Converts to string.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String" /> that represents this instance.
+        /// </returns>
+        public override string ToString()
+        {
+            return $"ProductID: - {ProductID}  UserID: - { UserID} Rating:- {Rating} Review:- { Review}  isLike:- { isLike}";
+        }
+    }
+}
