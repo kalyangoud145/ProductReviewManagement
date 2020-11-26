2c6279cbc3e93be249af0a096ad9d6238d92f928
kalyangoud145 <kalyangoud145@gmail.com>
2020-11-26 17:49:11 +0530

    [Kalyan] retrived all records whose islike is true

--- Management.cs
+++ Management.cs
@@ -119,5 +119,15 @@ namespace ProductReviewManagement
                 Console.WriteLine("Product ID: "+list.ProductID + " " + "Count: " + list.Review);
             }
         }
+        public void RetrieveTrueIsLike()
+        {
+            var Data = from reviews in dataTable.AsEnumerable()
+                       where reviews.Field<bool>("isLike").Equals(true)
+                       select reviews;
+            foreach (var dataItem in Data)
+            {
+                Console.WriteLine($"ProductID- {dataItem.ItemArray[0]} UserID- {dataItem.ItemArray[1]} Rating- {dataItem.ItemArray[2]} Review- {dataItem.ItemArray[3]} isLike- {dataItem.ItemArray[4]}");
+            }
+        }
     }
 }
